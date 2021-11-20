d57ae5c50bbd412258fd68caa6063161650b1cd4
bardbart <bartolomejonjeremiah@gmail.com>
2021-11-20 14:04:01 +0800

    minor fixes and additions

--- resources/views/documents/create.blade.php
+++ resources/views/documents/create.blade.php
@@ -36,95 +36,110 @@
                   </div>
               </div>
            @elseif ($hasCase == false)
-            <div class="card">
-               <div class="card-header font-weight-bold text-dark" style="background-color: #f6f7cd;">Document Request Form</div>
-               <div class="card-body">
-                  <form method="POST" action="{{ route('documents.store') }}" enctype="multipart/form-data">
-                     @csrf
-                     <label class="required" for="image">Valid ID</label>
-                     <div class="form-group mb-3">
-                           <input type="file" class="form-control" name="image" id="image" required>
-                           <span><b>Image must be .jpg / .jpeg / .png</b></span>
+           <div class="row">
+              
+               <div class="col-md-3">
+                  <div class="card">
+                     <div class="card-header font-weight-bold text-dark" style="background-color: #f6f7cd;">Note</div>
+                     <div class="card-body">
+                        <p class="card-text text-danger font-weight-bold">If any of your personal details have changed recently, refer to the Edit Profile to change them before Requesting a Document</p>
                      </div>
-                     <div class="form-group row mb-3">
-                           <div class="col-sm">
-                              <label class="required" for="lastName">Last Name</label>
-                              <input readonly type="text" class="form-control font-weight-bold" value="{{ Auth::user()->lastName }}" name="lastName" id="lastName" required>
+                  </div>
+               </div>
+
+               <div class="col-md-9">
+                  <div class="card">
+                     <div class="card-header font-weight-bold text-dark" style="background-color: #f6f7cd;">Document Request Form</div>
+                     <div class="card-body">
+                        <form method="POST" action="{{ route('documents.store') }}" enctype="multipart/form-data">
+                           @csrf
+                           <label class="required" for="image">Valid ID</label>
+                           <div class="form-group mb-3">
+                                 <input type="file" class="form-control" name="image" id="image" required>
+                                 <span><b>Image must be .jpg / .jpeg / .png</b></span>
+                           </div>
+                           <div class="form-group row mb-3">
+                                 <div class="col-sm">
+                                    <label class="required" for="lastName">Last Name</label>
+                                    <input readonly type="text" class="form-control font-weight-bold" value="{{ Auth::user()->lastName }}" name="lastName" id="lastName" required>
+                                 </div>
+                                 
+                                 <div class="col-sm">
+                                    <label class="required" for="firstName">First Name</label>
+                                    <input readonly type="text" class="form-control font-weight-bold" value="{{ Auth::user()->firstName }}" name="firstName" id="firstName" required>
+                                 </div>
+      
+                                 <div class="col-sm">
+                                    <label for="middleName">Middle Name</label>
+                                    @if (Auth::user()->middleName != null)                                  
+                                       <input readonly type="text" class="form-control font-weight-bold" value="{{ Auth::user()->middleName }}" name="middleName" id="middleName" required>
+                                    @else
+                                       <input readonly type="text" class="form-control font-weight-bold" value="None" name="middleName" id="middleName" required>
+                                    @endif
+                                 </div>
                            </div>
                            
-                           <div class="col-sm">
-                              <label class="required" for="firstName">First Name</label>
-                              <input readonly type="text" class="form-control font-weight-bold" value="{{ Auth::user()->firstName }}" name="firstName" id="firstName" required>
+                           <div class="form-group row mb-3">
+                                 <div class="col-sm">
+                                    <label class="required" for="citizenship">Citizenship</label>
+                                    <input readonly type="text" class="form-control font-weight-bold" value="{{ Auth::user()->citizenship }}" name="citizenship" id="citizenship" required>
+                                 </div>
+                                 <div class="col-sm">
+                                    <label class="required" for="houseNo">House Number</label>
+                                    <input readonly type="text" class="form-control font-weight-bold" value="{{ Auth::user()->houseNo }}" name="houseNo" id="houseNo" required>
+                                 </div>
+                                 <div class="col-sm">
+                                    <label class="required" for="street">Street</label>
+                                    <input readonly type="text" class="form-control font-weight-bold" value="{{ Auth::user()->street }}" name="street" id="street" required>
+                                 </div>
                            </div>
-
-                           <div class="col-sm">
-                              <label for="middleName">Middle Name</label>
-                              @if (Auth::user()->middleName != null)                                  
-                                 <input readonly type="text" class="form-control font-weight-bold" value="{{ Auth::user()->middleName }}" name="middleName" id="middleName" required>
-                              @else
-                                 <input readonly type="text" class="form-control font-weight-bold" value="None" name="middleName" id="middleName" required>
-                              @endif
+                           
+                           <div class="form-group row my-1">
+                              <div class="col-sm">
+                                 <label class="required" for="docType">{{ __('Document Type') }}</label>
+                                 <select class="form-control" name="docType" id="docType" required>
+                                    <option value>--Select Document Type--</option>
+                                 @foreach ($doctypes as $doctype) 
+                                    <option value="{{ $doctype->id }}">{{ $doctype->docType }} - ₱{{ $doctype->price }}</option>
+                                 @endforeach
+                                 </select>
+                              </div>
+                              <div class="col-sm">
+                                 <label class="required" for="purpose">{{ __('Purpose') }}</label>
+                                 <select class="form-control" name="purpose" id="purpose" onchange="showDiv('others', this)" required>
+                                    <option value>--Select Purpose--</option>
+                                    <option>Personal Identification and Residence Status</option>
+                                    <option>Good Standing in the Community</option>
+                                    <option>No pending case filed in the barangay</option>
+                                    <option>Employment (Local)</option>
+                                    <option>Employment (Abroad)</option>
+                                    <option>Enrollment</option>
+                                    <option>Scholarship</option>
+                                    <option>Senior Citizens & Solo Parent</option>
+                                    <option>Marriage (Local)</option>
+                                    <option>Marriage (Abroad)</option>
+                                    <option>Construction Permit</option>
+                                    <option>Construction Excavaion Permit</option>
+                                    <option value="others">Other Purpose</option>
+                                 </select>
+                              </div>
+                              <div class="col-sm" id="others" style="display: none">
+                                 <label for="others">Enter Other Purpose</label>
+                                 <input type="text" class="form-control" name="others" placeholder="Enter Other Purpose...">
+                              </div>
                            </div>
+      
+                           <div class="form-group float-right my-3">
+                              <button onclick="return confirm('Are your inputs correct?')" type="submit" class="btn btn-primary" >
+                                 {{ __('Submit') }}
+                              </button>
+                           </div>
+                        </form>     
                      </div>
-                     
-                     <div class="form-group row mb-3">
-                           <div class="col-sm">
-                              <label class="required" for="citizenship">Citizenship</label>
-                              <input readonly type="text" class="form-control font-weight-bold" value="{{ Auth::user()->citizenship }}" name="citizenship" id="citizenship" required>
-                           </div>
-                           <div class="col-sm">
-                              <label class="required" for="houseNo">House Number</label>
-                              <input readonly type="text" class="form-control font-weight-bold" value="{{ Auth::user()->houseNo }}" name="houseNo" id="houseNo" required>
-                           </div>
-                           <div class="col-sm">
-                              <label class="required" for="street">Street</label>
-                              <input readonly type="text" class="form-control font-weight-bold" value="{{ Auth::user()->street }}" name="street" id="street" required>
-                           </div>
-                     </div>
-                     
-                     <div class="form-group row my-1">
-                        <div class="col-sm">
-                           <label class="required" for="docType">{{ __('Document Type') }}</label>
-                           <select class="form-control" name="docType" id="docType" required>
-                              <option value>--Select Document Type--</option>
-                           @foreach ($doctypes as $doctype) 
-                              <option value="{{ $doctype->id }}">{{ $doctype->docType }} - ₱{{ $doctype->price }}</option>
-                           @endforeach
-                           </select>
-                        </div>
-                        <div class="col-sm">
-                           <label class="required" for="purpose">{{ __('Purpose') }}</label>
-                           <select class="form-control" name="purpose" id="purpose" onchange="showDiv('others', this)" required>
-                              <option value>--Select Purpose--</option>
-                              <option>Personal Identification and Residence Status</option>
-                              <option>Good Standing in the Community</option>
-                              <option>No pending case filed in the barangay</option>
-                              <option>Employment (Local)</option>
-                              <option>Employment (Abroad)</option>
-                              <option>Enrollment</option>
-                              <option>Scholarship</option>
-                              <option>Senior Citizens & Solo Parent</option>
-                              <option>Marriage (Local)</option>
-                              <option>Marriage (Abroad)</option>
-                              <option>Construction Permit</option>
-                              <option>Construction Excavaion Permit</option>
-                              <option value="others">Other Purpose</option>
-                           </select>
-                        </div>
-                        <div class="col-sm" id="others" style="display: none">
-                           <label for="others">Enter Other Purpose</label>
-                           <input type="text" class="form-control" name="others" placeholder="Enter Other Purpose...">
-                        </div>
-                     </div>
-
-                     <div class="form-group float-right my-3">
-                        <button onclick="return confirm('Are your inputs correct?')" type="submit" class="btn btn-primary" >
-                           {{ __('Submit') }}
-                        </button>
-                     </div>
-                  </form>     
+                  </div>
                </div>
-            </div>
+
+           </div>
            @endif
         </div>
     </div>
