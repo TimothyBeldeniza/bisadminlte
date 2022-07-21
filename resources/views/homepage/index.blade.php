799330e9ddbb4cd4748be97b2cf50d1c3069725a
TimothyBeldeniza <bsdtimothy@gmail.com>
2022-07-21 17:46:01 +0800

    fix the toggle button of navbar

--- resources/views/homepage/index.blade.php
+++ resources/views/homepage/index.blade.php
@@ -20,36 +20,23 @@
 </head>
 <body>
     <!-- START THE NAVBAR SECTION -->
-    <nav class="navbar navbar-expand-lg  navbar-dark menu shadow fixed-top">
+      <nav class="navbar navbar-expand-lg navbar-dark menu shadow fixed-top">
         <div class="container">
-          <a class="navbar-brand d-flex align-items-center" href="#">
-              <img class="image-fluid bis-logo" src="{{ asset('homepage-assets/images/BIS - Logo.png') }}" alt="logo-image">
-              <h3 class="fw-bold ms-2"> B I S</h3>
+          <a class="d-flex align-items-center navbar-brand" href="#">
+            <img src="{{ asset('homepage-assets/images/BIS - Logo.png') }}" alt="logo image">
+            <h3>B I S</h3>
           </a>
-          
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
           <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
             <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              <li class="nav-item"><a class="nav-link active" aria-current="page" href="#">Home</a></li>
+              <li class="nav-item"><a class="nav-link" href="#companies">About Us</a></li>
+              <li class="nav-item"><a class="nav-link" href="#testimonials">Announcements & Events</a></li>
+              <li class="nav-item"><a class="nav-link" href="#portfolio">Services</a></li>
+              <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">Login</a></li>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#companies">About Us</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#testimonials">Announcements & Events</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#portfolio">Services</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="{{ route('login') }}">Login</a>
-              </li>
-              <!-- <li class="nav-item">
-                <a class="nav-link" href="#"></a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#"></a>
-              </li> -->
             </ul>
           </div>
         </div>
